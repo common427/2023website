ff246d1af1f3bfbf14092a3b10f80ded0afa28c8
jiwongil <jiwongil@naver.com>
2023-11-20 17:35:30 +0900

    test100

--- src/components/Intersect.jsx
+++ src/components/Intersect.jsx
@@ -3,7 +3,8 @@ import {vData} from '../data.js'
 import {Container, Row, Col, Card} from 'react-bootstrap';
 
 function GalleryList(){
-    const [myData,setMyData] = useState(vData)
+    //const [myData,setMyData] = useState(vData)
+    const [myData] = useState(vData)
     return(
         <Container className='mb-2 mb-mb-5'>
             <Row>
