36dcc36de8352c34c823073e2a1a936523c3d5c0
jiwongil <jiwongil@naver.com>
2023-11-21 18:09:12 +0900

    ggyo55555

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 import App from './App';
-import {BrowserRouter} from 'react-router-dom'
 import reportWebVitals from './reportWebVitals';
 
 
@@ -14,6 +13,7 @@ root.render(
   </React.StrictMode>
 );
 
+//import {BrowserRouter} from 'react-router-dom'
 //<React.StrictMode>
 //  <BrowserRouter><App/></BrowserRouter>
 //</React.StrictMode>
