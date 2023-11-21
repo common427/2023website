a8e8e82a53d610a12eba94bc09dd6037d3d65222
jiwongil <jiwongil@naver.com>
2023-11-21 18:21:25 +0900

    ggyo55555

--- src/components/VisualView.jsx
+++ src/components/VisualView.jsx
@@ -2,8 +2,8 @@ import {Container} from 'react-bootstrap';
 
     function VisualView(){
         return(
-            <div className='visualWrap mb-3 d-flex justify-content-center justify-content-md-between'>
-                <Container className='py-4'>
+            <div className='visualWrap mb-3'>
+                <Container className='py-4 d-flex justify-content-center justify-content-md-between'>
                     <h2>문화를 만들고 인재를 키우고<br/> 마음을 나눕니다</h2>
                     <p>넷마블문화재단은 문화적 가치 확산을 통해 우리 사회의 미래 경쟁력을 높여 가고자 다양한 공익 활동을 전개해 나가고 있습니다.</p>
                 </Container>
