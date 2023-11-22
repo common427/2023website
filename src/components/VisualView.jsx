a955f313a70ffc19a2f0a2c40af4175290fa7041
jiwongil <jiwongil@naver.com>
2023-11-22 14:31:19 +0900

    ggyo55555

--- src/components/VisualView.jsx
+++ src/components/VisualView.jsx
@@ -5,7 +5,7 @@ import {Container} from 'react-bootstrap';
             <div className='visualWrap mb-3'>
                 <Container className='py-4 d-flex justify-content-center justify-content-md-between mx-0 mw-100'>
                     <h2>문화를 만들고 인재를 키우고<br/> 마음을 나눕니다</h2>
-                    <p className='pt-5 pl-1 pl-md-auto'>넷마블문화재단은 문화적 가치 확산을 통해 우리 사회의 미래 경쟁력을 높여 가고자 다양한 공익 활동을 전개해 나가고 있습니다.</p>
+                    <p className='pt-5 pl-2 pl-md-auto'>넷마블문화재단은 문화적 가치 확산을 통해 우리 사회의 미래 경쟁력을 높여 가고자 다양한 공익 활동을 전개해 나가고 있습니다.</p>
                 </Container>
             </div>
         )
